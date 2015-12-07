e1b6c114fd292424508c9a99db1a16805023613a
capital <capital.terefe@gmail.com>
2015-12-07 05:38:52 -0600

    new change

--- src/org/wp/NavigationTest.java
+++ src/org/wp/NavigationTest.java
@@ -1,6 +1,39 @@
 package org.wp;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class NavigationTest {
 
+	WebDriver driver;
+	
+	@Before
+	public void testSetup(){
+		
+		driver=new FirefoxDriver();
+		
+		
+	}
+	
+	
+	@After
+	public void tearDown(){
+		driver.close();
+		driver.quit();
+	}
+	
+	
+	
+	@Test
+	public void sanityTest(){
+		
+	}
+	
+	
+	
+	
 }
