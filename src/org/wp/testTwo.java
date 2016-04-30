3ee2928b961bea35bf47dee4f09b18187089edab
capitolkrs <capitolkrs@gmail.com>
2016-04-30 01:57:41 -0400

    all in one method

--- src/org/wp/testTwo.java
+++ src/org/wp/testTwo.java
@@ -3,17 +3,23 @@ package org.wp;
 public class testTwo {
  public static void main(String[] args) {
 	
-	 
-	 System.out.println("gitProject2");
+	
+ }
 
-	 System.out.println("TestBranch changes");
+private static void myPrints() {
+	{ 
+		 System.out.println("gitProject2");
 
-	 System.out.println("BaseSanityTest");
-	 
-	 System.out.println("Commit from terminal");
-int myInteger[]={1,2,3,4,5};
-	 for(int k:myInteger){
-		 System.out.println(k);
-	 }
+		 System.out.println("TestBranch changes");
+
+		 System.out.println("BaseSanityTest");
+		 
+		 System.out.println("Commit from terminal");
+	int myInteger[]={1,2,3,4,5};
+		 for(int k:myInteger){
+			 System.out.println(k);
+		 }
+	}
+	
 }
 }
